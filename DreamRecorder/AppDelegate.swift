6a4d432c066f8d3f431fc57c3093dc1f96c80d9c
jun-kb <preyoi403@gmail.com>
2025-12-10 01:34:47 +0900

    feat: Google Sign-In 基盤を追加
    - GoogleSignIn-iOS パッケージを追加
    - AppDelegate に URL ハンドリングを追加
    - Info.plist に REVERSED_CLIENT_ID の URL Scheme を設定

--- DreamRecorder/AppDelegate.swift
+++ DreamRecorder/AppDelegate.swift
@@ -1,10 +1,16 @@
 import SwiftUI
 import FirebaseCore
+import GoogleSignIn
 
 // Firebaseの初期設定
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         return true
+    }
+    
+    // Google Sign-In のコールバックURLを処理
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
     }
 }
